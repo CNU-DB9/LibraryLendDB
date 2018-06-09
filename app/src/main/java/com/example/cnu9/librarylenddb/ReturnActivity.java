c6d77d62eb95f32442410b3f0ad947b8db3231d4
KimHwanCheol <jkgbrj@naver.com>
2018-06-09 12:24:55 +0900

    반납 액티비티 추가 및 Book 클래스 생성하여 get, set 생성

---
+++ app/src/main/java/com/example/cnu9/librarylenddb/ReturnActivity.java
@@ -0,0 +1,15 @@
+package com.example.cnu9.librarylenddb;
+
+//빌린 목록 조회 페이지
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReturnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return);
+
+    }
+}

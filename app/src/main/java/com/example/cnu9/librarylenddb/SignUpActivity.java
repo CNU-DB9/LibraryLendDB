97a17cfb1edbf78c88f3e16c5783d1f4f8351e33
minjiyoo <1004yumj@naver.com>
2018-06-09 11:08:30 +0900

    로그인, 회원가입 화면 작성

--- app/src/main/java/com/example/cnu9/librarylenddb/SignUpActivity.java
+++ app/src/main/java/com/example/cnu9/librarylenddb/SignUpActivity.java
@@ -2,12 +2,60 @@ package com.example.cnu9.librarylenddb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editText_ID_SignUp;
+    private EditText editText_Password_SignUp;
+    private Button button_SignUp;
+    private Button button_Cancel;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editText_ID_SignUp = (EditText) findViewById(R.id.editText_ID_SignUp);
+        editText_Password_SignUp = (EditText) findViewById(R.id.editText_Password_SignUp);
+        button_SignUp = (Button) findViewById(R.id.button_SignUp);
+        button_Cancel = (Button) findViewById(R.id.button_Cancel);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        conditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        button_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conditionRef.setValue(editText_ID_SignUp.getText().toString());
+            }
+        });
     }
 }

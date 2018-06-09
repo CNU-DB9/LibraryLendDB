7616974d801865e29529c998b53a4bb3f74615f6
LeeHeungrok <rhh0208@naver.com>
2018-06-09 13:25:35 +0900

    대출 기본 화면 구성

---
+++ app/src/main/java/com/example/cnu9/librarylenddb/LendActivity.java
@@ -0,0 +1,13 @@
+package com.example.cnu9.librarylenddb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lend);
+    }
+}

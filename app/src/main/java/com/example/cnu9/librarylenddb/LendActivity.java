9fcc2d0cce21c1d63bdf1ca31725baec5f5315f8
LeeHeungrok <rhh0208@naver.com>
2018-06-09 16:36:15 +0900

    검색하여 책 가져오는거 완료

--- app/src/main/java/com/example/cnu9/librarylenddb/LendActivity.java
+++ app/src/main/java/com/example/cnu9/librarylenddb/LendActivity.java
@@ -9,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,15 +37,22 @@ public class LendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lend);
 
-        listView = (ListView) findViewById(R.id.productListView);
+        listView = findViewById(R.id.productListView);
         adapter = new BookAdapter();
-        btnFind = (Button) findViewById(R.id.btn_find);
-        inputFind = (EditText) findViewById(R.id.input_find);
+        btnFind = findViewById(R.id.btn_find);
+        inputFind = findViewById(R.id.input_find);
 
         btnFind.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String bookName = inputFind.getText().toString();
 
+                if(bookName.isEmpty()){
+                    toastMessage("책 이름을 입력해주세요!");
+                }
+                else {
+                    searchBookList(bookName);
+                }
             }
         });
 
@@ -119,15 +127,28 @@ public class LendActivity extends AppCompatActivity {
         });
     }
 
-    private void searchBookList(String bookName){
+    private void searchBookList(final String bookName){
+        Log.d("SearchBookList", bookName);
         book.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 adapter.allRemove();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    Log.d("Test", "접속함.");
-                    Log.d("Test", snapshot.getKey());
+                    Log.d("SearchBookList For문", snapshot.child("BookName").getValue(String.class));
+                    if(bookName.equals(snapshot.child("BookName").getValue(String.class))){
+                        Log.d("SearchBookList For문", "if문 확인");
+                        String bookCode = snapshot.child("BookCode").getValue(String.class);
+                        String bookName = snapshot.child("BookName").getValue(String.class);
+                        String author = snapshot.child("Author").getValue(String.class);
+                        String bookPublisher = snapshot.child("BookPublisher").getValue(String.class);
+                        adapter.addItem(new Book(bookCode, bookName, author, bookPublisher));
+                    }
+                    Log.d("SearchBookList For문", snapshot.getKey());
                 }
+                if(adapter.getCount() == 0){
+                    toastMessage("책이 검색되지 않습니다!");
+                }
+                listView.setAdapter(adapter);
             }
 
             @Override
@@ -135,5 +156,10 @@ public class LendActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void toastMessage(String msg){
+        Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }

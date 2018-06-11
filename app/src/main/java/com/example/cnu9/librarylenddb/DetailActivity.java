ef24776a382f5dd836af3bab8b45e212b6cecf7b
LeeHeungrok <rhh0208@naver.com>
2018-06-11 16:51:26 +0900

    반납일 추가 및 디테일 화면 재구성

--- app/src/main/java/com/example/cnu9/librarylenddb/DetailActivity.java
+++ app/src/main/java/com/example/cnu9/librarylenddb/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,12 +17,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DetailActivity extends AppCompatActivity {
 
-    TextView text_bookName, text_author, text_bookPublisher, text_lendDate;
+    TextView text_bookName, text_author, text_bookPublisher, text_lendDate, text_returnDate;
+    LinearLayout lendDate_Group, returnDate_Group, isLend;
     Button returnButton, lendButton;
     SharedPreferences pref;
     String id;
@@ -48,6 +52,10 @@ public class DetailActivity extends AppCompatActivity {
         text_author = (TextView) findViewById(R.id.author);
         text_bookPublisher = (TextView) findViewById(R.id.bookPublisher);
         text_lendDate = (TextView) findViewById(R.id.lendDate);
+        text_returnDate = findViewById(R.id.returnDate);
+        lendDate_Group = findViewById(R.id.lendGroup);
+        returnDate_Group = findViewById(R.id.returnGroup);
+        isLend = findViewById(R.id.isLend);
 
         lendButton = (Button) findViewById(R.id.lendButton);
         returnButton = (Button) findViewById(R.id.returnButton);
@@ -65,12 +73,16 @@ public class DetailActivity extends AppCompatActivity {
         // 대출일 때,
         if(previousActivity == 1){
             returnButton.setVisibility(View.INVISIBLE);
+            lendDate_Group.setVisibility(View.INVISIBLE);
+            returnDate_Group.setVisibility(View.INVISIBLE);
+            isLend.setVisibility(View.INVISIBLE);
             mBook.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     Boolean stock = dataSnapshot.child(bookCode).child("stock").getValue(Boolean.class);
                     if(!stock){
                         lendButton.setVisibility(View.INVISIBLE);
+                        isLend.setVisibility(View.VISIBLE);
                     }
                 }
 
@@ -82,11 +94,23 @@ public class DetailActivity extends AppCompatActivity {
         }
         else if(previousActivity == 2){
             lendButton.setVisibility(View.INVISIBLE);
+            lendDate_Group.setVisibility((View.VISIBLE));
+            isLend.setVisibility(View.INVISIBLE);
             mUser.child(id).addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
+                    SimpleDateFormat dayTime = new SimpleDateFormat("yyyy.MM.dd");
                     String lendDate = dataSnapshot.child("LendBookCode").child(bookCode).getValue(String.class);
-                    text_lendDate.setText(lendDate);
+                    if(lendDate != null) {
+                        ParsePosition pp = new ParsePosition(0);
+                        Date date = dayTime.parse(lendDate, pp);
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(date);
+                        cal.add(Calendar.DATE, 7);
+
+                        text_lendDate.setText(lendDate);
+                        text_returnDate.setText(dayTime.format(cal.getTime()));
+                    }
                 }
 
                 @Override
